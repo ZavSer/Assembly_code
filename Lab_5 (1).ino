aaac84d172943c98e05e28c5f0e22f055775e236
ZavSer <ma.zavalaserret@gmail.com>
2018-03-13 20:58:17 -0400

    Add files via upload

---
+++ Lab_5 (1).ino
@@ -0,0 +1,79 @@
+#include <mechbotShield.h>
+#include <avr/io.h>
+
+int main(void){
+ 
+  initSoftSerial();
+  setLCDBackLight(147);
+  clrLCD();
+  moveLCDCursor(0);
+  initADC();
+
+    uint16_t inputAnalog = 0;
+    uint8_t buttonPress = 0;
+  DDRD &= ~((1 << PD3) | (1 << PD4) | (1 << PD5));
+  DDRC &= ~(1 << PC1);
+  PORTD |= (1 << PD3) | (1 << PD4) | (1 << PD5);
+  PORTC |= (1 << PC1);
+  DDRB |= 0b00111111;
+  DDRD |= (1 << PD6) | (1 << PD7);
+  PORTB |= 0b00000000;
+  PORTD &= ~((1 << PD6) | (1 << PD7));
+
+  while (1){
+ 
+  delay_ms(200);
+   if((PINCrw & (1 << PC1)) == 0){
+      buttonPress++;
+      if(buttonPress == 7){
+        buttonPress = 0;
+        
+      }
+      inputAnalog = analog(buttonPress);
+  
+      do{
+        //Loop 
+      }while((PIND & (1 << PD4)) == 0);
+    }
+    
+      inputAnalog = analog(buttonPress);
+      clrLCD();
+    
+    if(buttonPress == 0){
+        lcdPrint("line_1=");
+    lcdPrintDec(inputAnalog);
+    moveLCDCursor(0);
+    }
+    
+    if(buttonPress == 1){
+        lcdPrint("line_2 =");
+    lcdPrintDec(inputAnalog);
+    moveLCDCursor(0);
+    }
+  if(buttonPress == 2){
+        lcdPrint("line_3=");
+    lcdPrintDec(inputAnalog);
+    moveLCDCursor(0);
+    }
+      if(buttonPress == 3){
+        lcdPrint("line_4=");
+    lcdPrintDec(inputAnalog);
+    moveLCDCursor(0);
+    }
+      if(buttonPress == 4){
+        lcdPrint("Dis_left=");
+    lcdPrintDec(inputAnalog);
+    moveLCDCursor(0);
+    }
+      if(buttonPress == 5){
+        lcdPrint("Dis_center=");
+    lcdPrintDec(inputAnalog);
+    moveLCDCursor(0);
+    }
+      if(buttonPress == 6){
+        lcdPrint("Dis_right=");
+    lcdPrintDec(inputAnalog);
+    moveLCDCursor(0);
+    }
+  }
+}
---
+++ Week4_Example_PUshbutton.ino
@@ -0,0 +1,13 @@
+#include <mechbotShield.h>
+#include <USART.h>
+#include <avr/io.h>
+#include "Week4_Example_PUshbutton.h"
+
+int main()  
+{  
+     push_button_init();
+     led_init();
+     push_button_1();
+
+     return 0;
+ }
